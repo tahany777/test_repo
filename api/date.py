2a8d5e3170c658a7aae86bc6e21806aef422d247
tahany777 <tahanymohammed777@hotmail.com>
2022-10-30 09:21:49 +0200

    -----

--- api/date.py
+++ api/date.py
@@ -1,12 +1,11 @@
 from http.server import BaseHTTPRequestHandler
-from cowpy import cow
+from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
-        self.wfile.write(message.encode())
-        return
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    return
